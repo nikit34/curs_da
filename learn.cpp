244bb85af14b40b438187f51abe0dda214c417d1
nikit34 <permikov134@mail.ru>
2021-05-30 13:09:25 +0300

    gitignore

--- learn.cpp
+++ learn.cpp
@@ -1,6 +1,6 @@
 #include "learn.h"
 
-uint32_t TOTAL_TRAINING;
+uint32_t TOTAL_TRAINING = 0;
 std::unordered_map<std::string, class_statistic> CLASSES;
 
 inline void set_empty_input(uint32_t& num_line, std::vector<std::string>& tags, std::string& title, std::string& text) {
@@ -20,11 +20,12 @@ void get_tags(std::ifstream& file, std::string& line, std::vector<std::string>& 
     int32_t pos = 0;
     std::string token;
     while ((pos = line.find(s)) != std::string::npos) {
-        token = s.substr(0, pos);
+        token = line.substr(0, pos);
         tags.push_back(token);
         line.erase(0, pos + 1);
     }
     tags.push_back(token);
+    std::cout << token << std::endl;
 }
 
 inline void get_title(std::ifstream& file, std::string& title){
@@ -71,7 +72,7 @@ void start_learn(std::vector<std::string>& tags, std::string& title, std::string
 
 void learn(std::string& text, std::string& cls) {
     class_statistic cls_stat;
-    //++TOTAL_TRAINING;
+    ++TOTAL_TRAINING;
 
     if (CLASSES.count(cls)) {
         cls_stat = CLASSES[cls];
@@ -89,7 +90,6 @@ void learn(std::string& text, std::string& cls) {
         ++cls_stat.words[token];
         ++cls_stat.total_words;
     }
-
     CLASSES[cls] = cls_stat;
 }
 
@@ -109,7 +109,7 @@ std::string& classify(std::string& text) {
     class_statistic cls_stat;
 
     for (auto& cls : CLASSES) {
-        name_class = cls.first;
+        name_class = cls.first;  // TODO: read from file
         cls_stat = cls.second;
         laplace_smoothing = cls_stat.total_words;
 
@@ -128,7 +128,7 @@ std::string& classify(std::string& text) {
             count = item_word_count.second;
 
             if (cls_stat.words.count(word)) {
-                //metric_class += count * (log(cls_stat.words[word]) - log(laplace_smoothing));
+                metric_class += count * (log(cls_stat.words[word]) - log(laplace_smoothing));
             }
             else {
                 metric_class += count * (-log(laplace_smoothing));
@@ -146,9 +146,15 @@ std::string& classify(std::string& text) {
 void write_statistic(std::string& name_file) {
     std::ofstream file;
     file.open(name_file);
-    //file << TOTAL_TRAINING;
+    file << TOTAL_TRAINING << std::endl;
     for (auto& cls : CLASSES) {
         file << cls.first;
+        file << "," << cls.second.count_outdoor;
+        file << "," << cls.second.total_words;
+        for (auto& token : cls.second.words){
+            file << "," << token.first << ":" << token.second;
+        }
+        file << std::endl;
     }
     file.close();
 }
